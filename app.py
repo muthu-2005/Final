0ee129b83b15ecb29ee3ddf03c9045abb7af8626
MUTHUKUMAR G <139162239+muthu-2005@users.noreply.github.com>
2024-12-08 19:09:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,17 @@ app = Flask(__name__)
 # Generate a secure random secret key (or you can use an environment variable)
 app.secret_key = secrets.token_hex(16)  # Generates a 32-character hex string
 
-# Initialize Firebase Admin SDK
-cred = credentials.Certificate('pass.json')  # Update with the path to your service account file
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://dopeshield-fa348-default-rtdb.firebaseio.com/'  # Your Firebase Realtime Database URL
-})
+# Get Firebase credentials from the environment variable
+firebase_credentials = os.getenv('FIREBASE_CREDENTIALS')
+
+if firebase_credentials:
+    # Load the Firebase credentials from the environment variable
+    cred = credentials.Certificate(json.loads(firebase_credentials))
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://dopeshield-fa348-default-rtdb.firebaseio.com/'  # Your Firebase Realtime Database URL
+    })
+else:
+    raise ValueError("Firebase credentials not found in environment variables!")
 
 
 #################
